965ee1dd898f2e700f49338d94b15bda755281ad
meshulamer <meshulamer@gmail.com>
2021-11-30 15:08:56 +0200

    Initial commit, parse hpp file and added parser, scanner files

--- output.hpp
+++ output.hpp
@@ -1,1 +1,9 @@
-#ifndef _236360_2_ #define _236360_2_ #include namespace output { extern const std::string rules[]; void printProductionRule(const int ruleno); void errorLex(const int lineno); void errorSyn(const int lineno); }; #endif
+#ifndef _236360_2_
+#define _236360_2_
+#include namespace output
+{
+    extern const std::string rules[];
+    void printProductionRule(const int ruleno);
+    void errorLex(const int lineno);
+    void errorSyn(const int lineno);
+}; #endif
